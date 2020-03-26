de7ec7b0f57680111d229272bb4e02e0f13b71fd
iCodeJavascript <frejcodes@gmail.com>
2020-03-26 15:15:12 +0000

    Updates

---
+++ precache-manifest.34b9be39c2a574b20c9e3f89fccdbf88.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d7e29fd1249b4d47df447ec3358721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb284ea023a19646e01b",
+    "url": "/static/css/main.bac9a870.chunk.css"
+  },
+  {
+    "revision": "c74db00df9bbdc0599db",
+    "url": "/static/js/2.61d5dc05.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.61d5dc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb284ea023a19646e01b",
+    "url": "/static/js/main.c1b3ed18.chunk.js"
+  },
+  {
+    "revision": "2b9cfd464e62782a650c",
+    "url": "/static/js/runtime-main.56f36095.js"
+  },
+  {
+    "revision": "514325ece003921058fc06e02264b2af",
+    "url": "/static/media/FreigTexMed.514325ec.woff2"
+  },
+  {
+    "revision": "155b4563d0a0c84280b87a8aef015047",
+    "url": "/static/media/FreigTexMedIta.155b4563.woff"
+  },
+  {
+    "revision": "877fa9c75a92b2c7334e5c758942da49",
+    "url": "/static/media/FreigTexMedIta.877fa9c7.woff2"
+  },
+  {
+    "revision": "f2314ac99e7bd09beb2063f025f7e158",
+    "url": "/static/media/FreightTextProBook.f2314ac9.woff"
+  },
+  {
+    "revision": "25e19141a0424b8ed25362d1bb234639",
+    "url": "/static/media/SimplonNorm-Regular-WebS.25e19141.woff2"
+  },
+  {
+    "revision": "31f38cf0892221cfd377160625876419",
+    "url": "/static/media/SimplonNorm-Regular-WebS.31f38cf0.woff"
+  },
+  {
+    "revision": "582c4ec95f7f8e12923d4184794bce1b",
+    "url": "/static/media/simplonnorm-bold-webfont.582c4ec9.woff"
+  },
+  {
+    "revision": "ef3178e314dbd0811546041e3ab0fbdf",
+    "url": "/static/media/simplonnorm-bold-webfont.ef3178e3.woff2"
+  },
+  {
+    "revision": "20057a357a3499925f44d33d6efe60db",
+    "url": "/static/media/simplonnorm-medium-webfont.20057a35.woff2"
+  },
+  {
+    "revision": "b60da1c99b18888831e7259752a17398",
+    "url": "/static/media/simplonnorm-medium-webfont.b60da1c9.woff"
+  }
+]);
